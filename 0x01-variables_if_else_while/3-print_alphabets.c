ec6d1221e2570ef1fb50d20e8973f4b97ebc34e0
ZainabRaheem <zainabraheemoluwafunke@gmail.com>
2022-04-08 15:07:05 -0700

    Task 3 commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,12 @@
  */
 int main(void)
 {
-        int ch;
+	int ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-	{
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	}
-        for (ch = 'A'; ch <= 'Z'; ch++)
-	{
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-	}
-        putchar('\n');
-        return (0);
-} 
+	putchar('\n');
+	return (0);
+}
